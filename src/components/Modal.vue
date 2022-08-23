74237c9d0a0acbd6d0d3edb9e77177e7c3b8bf7d
Kelvin Nguyen <knguyen@csedev-knguyen.bart.gov>
2022-08-22 17:05:06 -0700

    Started working through module 10
    Module 10 - Child components, props, and emits

---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>This is our modal</h1>
+            <slot />
+            <button>Hide modal</button>
+        </div>
+    </teleport>
+</template>
+
+
+
+<style scoped>
+.modal {
+    background-color: darkolivegreen;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,32 +2,15 @@
     <div id="modal">
         <h1>Modals</h1>
         <button @click="showModal = true">Show modal</button>
-
-        <teleport to=".modals-container">
-            <div v-if="showModal" class="modal">
-                <h1>This is our modal</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, molestias!</p>
-                <button @click="showModal = false">Hide modal</button>
-            </div>
-        </teleport>
+        <Modal v-if="showModal">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, yeet.</p>
+        </Modal>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 // modal
-const showModal = ref(true);
+const showModal = ref(false);
 </script>
-
-<style scoped>
-.modal {
-    background-color: darkolivegreen;
-    padding: 10px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-}
-</style>
